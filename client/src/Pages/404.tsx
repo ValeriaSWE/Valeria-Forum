d620b1f98ebef3b2f2a882074970bf441eb3e093
Grizly1112 <81764115+Grizly1112@users.noreply.github.com>
2022-09-18 22:27:37 +0200

    404 UI
    Minor UI update to 404 page

--- client/src/Pages/404.tsx
+++ client/src/Pages/404.tsx
@@ -1,8 +1,17 @@
+import Navbar from "../Components/Navbar";
+import styles from './StylingModules/404.module.css';
+
 export default function NotFound() {
   document.title = "Valeria Roleplay | 404";
   return (
   <>
-    <h1>404</h1>
-    <h1>Sidan finns inte</h1>
+   <Navbar />
+   <div class={styles.pageNotFound}>
+    <div class={styles.content}>
+      <i class="material-icons">error</i>
+      <h1>Sidan du söker verkar <br /> inte finnas.</h1>
+      <a onClick={() => {history.back()}}>Tillbaka</a>
+    </div>
+   </div>
   </>)
 }
